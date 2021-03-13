5ee409fdc9299e520d3a2e4801b88318a6b5aa39
전은형 <ooooo5h91@gmail.com>
2021-03-13 14:01:45 +0900

    확인버튼 클릭시 => 새 닉네임을 들고 복귀

--- app/src/main/java/com/neppplus/intent_20210313/EditNicknameActivity.kt
+++ app/src/main/java/com/neppplus/intent_20210313/EditNicknameActivity.kt
@@ -1,11 +1,34 @@
 package com.neppplus.intent_20210313
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nickname)
+
+//        확인 버튼이 눌리면 => 입력한 새 닉네임을 가지고 복귀
+        okBtn.setOnClickListener {
+
+//            입력한 닉네이을 추출 (변수 저장)
+            val inputNewNickname = nicknameEdt.text.toString()
+
+//            닉네임을 담아주자
+            val resultIntent = Intent() // 왕복이므로 출발/도착 정보 필요 없다 => 단순 데이터 첨부용 Intent
+            resultIntent.putExtra("nick", inputNewNickname)
+
+//            OK를 눌렀다고 결과 세팅 + 첨부 데이터는 resultIntent에 있다.
+            setResult(Activity.RESULT_OK, resultIntent)
+
+//            복귀
+            finish()
+
+        }
+
+
     }
 }
