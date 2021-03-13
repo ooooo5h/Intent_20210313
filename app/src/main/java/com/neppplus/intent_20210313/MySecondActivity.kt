ad99f62469fc9b023094d46935c58ef5f0e1dff3
전은형 <ooooo5h91@gmail.com>
2021-03-13 12:38:32 +0900

    데이터 넘겨주기 연습용 화면

---
+++ app/src/main/java/com/neppplus/intent_20210313/MySecondActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.intent_20210313
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MySecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_second)
+    }
+}
