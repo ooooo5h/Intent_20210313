97223c29efdff031a199c368ca4850a59066e1b0
전은형 <ooooo5h91@gmail.com>
2021-03-13 12:23:52 +0900

    인텐트로 다른 화면 이동

--- app/src/main/java/com/neppplus/intent_20210313/MainActivity.kt
+++ app/src/main/java/com/neppplus/intent_20210313/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.neppplus.intent_20210313
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveToFirstBtn.setOnClickListener {
+
+            val myIntent = Intent(this, MyFirstActivity::class.java)
+            startActivity(myIntent)
+
+        }
+
     }
 }
