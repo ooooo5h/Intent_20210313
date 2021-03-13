913f6c26b4a9d6a1ef12a42f48d7da6a9a466a9d
전은형 <ooooo5h91@gmail.com>
2021-03-13 13:42:02 +0900

    닉네임 입력하는 화면 추가햇음

--- app/src/main/java/com/neppplus/intent_20210313/MainActivity.kt
+++ app/src/main/java/com/neppplus/intent_20210313/MainActivity.kt
@@ -13,12 +13,9 @@ class MainActivity : AppCompatActivity() {
         moveToFirstBtn.setOnClickListener {
 
 //            출발지/목적지 정보 : 비행기 티켓 발권.
-
             val myIntent = Intent(this, MyFirstActivity::class.java)
-
 //            티켓 들고 비행기 탑승
             startActivity(myIntent)
-
         }
 
 
@@ -26,16 +23,13 @@ class MainActivity : AppCompatActivity() {
 
 //            어떤 내용이 적혔는지?
             val inputMessage = messageEdt.text.toString()
-
-
-//            화면 이동인건 동일하니 intent 사용볍 그대로 적용
+//            화면 이동인건 동일하니 intent 사용법 그대로 적용
 
 //            비행기 티켓
-            val myIntent : Intent(this, MySecondActivity::class.java)
+            val myIntent = Intent(this, MySecondActivity::class.java)
 
 //            티켓에 데이터 추가 기록.
             myIntent.putExtra("message",inputMessage)
-
 
 //            실제 탑승
             startActivity(myIntent)
